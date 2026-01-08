074c931a437e14f945289fcba83ce7ae9b69362f
lineside0418 <maincraft0418yuta@gmail.com>
2026-01-08 22:40:14 +0900

    update haptic feedback

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mimi-roulette-v4'; // ★更新するたびにここを v4, v5... と変えるのが作法
+const CACHE_NAME = 'mimi-roulette-v5'; // ★更新するたびにここを v4, v5... と変えるのが作法
 const urlsToCache = [
   './',
   './index.html',
